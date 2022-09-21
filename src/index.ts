0edf1040071a249d2b8f48ff626013c4a4a28a40
Norikiru <norikiru.official@gmail.com>
2022-09-21 21:47:03 +0800

    pup: remove --no-zygote from args to stop crashing

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ let page: Page
                 `--disable-dev-shm-usage`,
                 `--disable-accelerated-2d-canvas`,
                 `--no-first-run`,
-                `--no-zygote`,
                 `--single-process`,
                 `--disable-gpu`,
                 `--disable-extensions`
