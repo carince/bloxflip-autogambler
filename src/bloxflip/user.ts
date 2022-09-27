17c595d7de71313bb5fc86796f0ba4d210da638a
Norikiru <norikiru.official@gmail.com>
2022-09-27 16:51:09 +0800

    user: add webhook

--- src/bloxflip/user.ts
+++ src/bloxflip/user.ts
@@ -1,6 +1,7 @@
 import { page } from '../index';
 import chalk from 'chalk';
 import { Logger } from '../utils/logger';
+import { sendWh } from '../utils/webhook';
 
 async function checkAuth() {
     Logger.info(`USER`, `\tFetching user information.`);
@@ -14,6 +15,37 @@ async function checkAuth() {
 
         if (res.success) {
             Logger.log(`USER`, `\t${chalk.bold(`User Information`)} \n\t\tUsername: ${res.user.robloxUsername} \n\t\tID: ${res.user.robloxId}\n\t\tBalance: ${Math.round((res.user.wallet + Number.EPSILON) * 100) / 100} R$`);
+            sendWh({
+                'embeds': [
+                  {
+                    'title': 'Successfully logged in!',
+                    'color': 3092790,
+                    'fields': [
+                      {
+                        'name': 'Username',
+                        'value': res.user.robloxUsername,
+                        'inline': true
+                      },
+                      {
+                        'name': 'Roblox ID',
+                        'value': res.user.robloxId,
+                        'inline': true
+                      },
+                      {
+                        'name': 'Balance',
+                        'value': `${Math.round((res.user.wallet + Number.EPSILON) * 100) / 100} R$`,
+                        'inline': true
+                      }
+                    ],
+                    'footer': {
+                      'text': 'bloxflip-autocrash'
+                    },
+                    'thumbnail': {
+                      'url': `https://www.roblox.com/headshot-thumbnail/image?userId=${res.user.robloxId}&width=720&height=720`
+                    }
+                  }
+                ]
+              })
         } else {
             Logger.error(`TOKEN`, `Invalid token provided, please put a valid token into the config.`, true);
         }
