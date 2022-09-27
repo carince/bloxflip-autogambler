d25325b464a23b07927b4abd6d386d7f5c23d0fb
Norikiru <norikiru.official@gmail.com>
2022-09-27 16:17:05 +0800

    webhook: fix webhook and warn when it errors

--- src/utils/webhook.ts
+++ src/utils/webhook.ts
@@ -1,14 +1,16 @@
 import fetch from 'node-fetch'
 import { config } from './config';
+import { Logger } from './logger';
 
-async function sendWh(json: any) {
+async function sendWh(json: {}) {
     const content = JSON.stringify(json)
 
     if (config.webhook.enabled) {
         await fetch(config.webhook.link, {
             method: 'post',
+            headers: {'Content-Type': 'application/json'}, 
             body: content
-        }).then(res => console.log(res))
+        }).catch(() => Logger.warn(`WEBHOOK`, `Failed to send message to webhook.`))
     }
 }
 
