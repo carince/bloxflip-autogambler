84d6870fb91888800e24b9d20f641050b5be08e4
Norikiru <norikiru.official@gmail.com>
2022-10-01 12:53:08 +0800

    config: use json5 to be able to use comments readme: remove "remove comments" instruction *: linted all files

--- src/bloxflip/crash.ts
+++ src/bloxflip/crash.ts
@@ -1,37 +1,36 @@
-import { page } from '../index';
-import { bet } from './bet';
-import { Logger } from '../utils/logger';
-import { checkAuth } from './user';
-import { getInfo } from './data'
-import { config } from '../utils/config';
-import { startRain } from './rain'
+import { page } from "../index";
+import { bet } from "./bet";
+import { Logger } from "../utils/logger";
+import { checkAuth } from "./user";
+import { getInfo } from "./data";
+import { config } from "../utils/config";
+import { startRain } from "./rain";
 
-let gameLoss = 0
-let gameWon = 0
-let gameCount = gameLoss + gameWon
+let gameLoss = 0;
+let gameWon = 0;
 
 async function startCrash() {
     await checkAuth();
 
-    if (config.webhook.modules.analytics) getInfo()
-    if (config.webhook.modules.rain.enabled) startRain()
+    if (config.webhook.modules.analytics) getInfo();
+    if (config.webhook.modules.rain.enabled) startRain();
 
-    Logger.info(`BLOXFLIP`, `Waiting for network idle...`);
+    Logger.info("BLOXFLIP", "Waiting for network idle...");
     await page.waitForNetworkIdle({ timeout: 60000 });
-    Logger.info(`BLOXFLIP`, `Starting autocrash.`);
+    Logger.info("BLOXFLIP", "Starting autocrash.");
 
-    const elementArr = [`div.gameBlock.gameBet.crash_crashBet__D5Rs_ > button`, `input.input_input__uGeT_.input_inputWithCurrency__sAiOQ`, `div.header_headerUserBalance__UEAJq`, `div.crash_crashGameCoefficient__M8rxs`, `input.input_input__uGeT_`];
+    const elementArr = ["div.gameBlock.gameBet.crash_crashBet__D5Rs_ > button", "input.input_input__uGeT_.input_inputWithCurrency__sAiOQ", "div.header_headerUserBalance__UEAJq", "div.crash_crashGameCoefficient__M8rxs", "input.input_input__uGeT_"];
     for (const element of elementArr) {
         if (!await page.$(element)) {
-            Logger.error(`ELEMENTS`, `Unable to query the element: ${element}`);
+            Logger.error("ELEMENTS", `Unable to query the element: ${element}`);
         }
     }
-    Logger.info(`ELEMENTS`, `Queried all elements.`);
+    Logger.info("ELEMENTS", "Queried all elements.");
 
-    const betMulti = (await page.$$(`input.input_input__uGeT_`))[1];
-    await betMulti.type(`2`);
+    const betMulti = (await page.$$("input.input_input__uGeT_"))[1];
+    await betMulti.type("2");
 
-    Logger.info(`CRASH`, `Starting crash bot`);
+    Logger.info("CRASH", "Starting crash bot");
 
     let cashed = false;
     let lossStreak = 0;
@@ -39,28 +38,28 @@ async function startCrash() {
     async function start() {
         new Promise(() => {
             setTimeout(async () => {
-                const textContent = await page.$eval(`div.crash_crashGameCoefficient__M8rxs`, e => e.textContent);
-                const className = await page.$eval(`div.crash_crashGameCoefficient__M8rxs`, e => e.className);
+                const textContent = await page.$eval("div.crash_crashGameCoefficient__M8rxs", e => e.textContent);
+                const className = await page.$eval("div.crash_crashGameCoefficient__M8rxs", e => e.className);
 
-                if (textContent?.includes(`+`)) {
-                    if (className.includes(`isCrashed`)) {
+                if (textContent?.includes("+")) {
+                    if (className.includes("isCrashed")) {
                         cashed = false;
                         gameLoss++;
                         lossStreak++;
-                        Logger.log(`CRASH`, `Status: Lost \n `);
-                        Logger.log(`GAME`, `\tGame #${gameCount}`);
-                        Logger.log(`GAME`, `\tLoss Streak: ${lossStreak}`);
+                        Logger.log("CRASH", "Status: Lost \n ");
+                        Logger.log("GAME", `\tGame #${gameLoss+gameWon}`);
+                        Logger.log("GAME", `\tLoss Streak: ${lossStreak}`);
                         await sleep(3000);
                         await bet(false);
                     }
 
-                    if (className.includes(`isCashed`)) {
+                    if (className.includes("isCashed")) {
                         if (!cashed) {
                             cashed = true;
                             gameWon++;
                             lossStreak = 0;
-                            Logger.log(`CRASH`, `Status: Won \n `);
-                            Logger.log(`GAME`, `\tGame #${gameCount}`);
+                            Logger.log("CRASH", "Status: Won \n ");
+                            Logger.log("GAME", `\tGame #${gameLoss+gameWon}`);
                             await sleep(3000);
                             await bet(true);
                         }
@@ -68,17 +67,17 @@ async function startCrash() {
                         cashed = false;
                     }
                 } else {
-                    if (className.includes(`isCrashed`)) {
-                        Logger.warn(`CRASH`, `Haven't joined this game, ignoring...`);
+                    if (className.includes("isCrashed")) {
+                        Logger.warn("CRASH", "Haven't joined this game, ignoring...");
                         if (lossStreak) {
-                            Logger.warn(`CRASH`, `Continuing loss streak, getting wiped is possible.`);
+                            Logger.warn("CRASH", "Continuing loss streak, getting wiped is possible.");
                             await sleep(3000);
                             await bet(false);
                         } else {
                             await sleep(3000);
                             await bet(true);
                         }
-                        console.log(`\n`)
+                        console.log("\n");
                     }
                 }
 
@@ -94,4 +93,4 @@ function sleep(ms: number) {
     });
 }
 
-export { startCrash, gameLoss, gameWon, gameCount };
+export { startCrash, gameLoss, gameWon };
--- src/utils/webhook.ts
+++ src/utils/webhook.ts
@@ -1,13 +1,13 @@
-import { curly as curl } from 'node-libcurl'
-import { config } from './config';
+import { curly as curl } from "node-libcurl";
+import { config } from "./config";
 
-async function sendWh(json: {}) {
+async function sendWh(json: any) {
     if (config.webhook.enabled) {
         await curl.post(config.webhook.link, {
             postFields: JSON.stringify(json),
-            httpHeader: ['Content-Type: application/json', 'Accept: application/json'],
+            httpHeader: ["Content-Type: application/json", "Accept: application/json"],
             sslVerifyPeer: false
-        })
+        });
     }
 }
 
