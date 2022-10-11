4dbb2cf5d6926e2f18cc7d5ce034cea218a14c81
nikosszzz <19nikospap@gmail.com>
2022-10-11 16:26:18 +0300

    index: add branch const

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import { sleep } from "./utils/sleep";
 import { initialize } from "./utils/browser";
 import { updater } from "./utils/updater";
 export let page: Page;
+export const branch = "stable";
 
 (async (): Promise<void> => {
     await updater();
 
-    Logger.log("STARTUP", "Starting bloxflip-autocrash");
+    Logger.log("STARTUP", `Starting bloxflip-autocrash (${branch})`);
     Logger.log("SUPPORT", "Support the developers by giving the repo a star! https://github.com/Norikiru/bloxflip-autocrash");
 
     await sleep(1000);
