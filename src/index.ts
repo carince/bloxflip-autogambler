c2074294fc3f03a2917d8a5185eb3c93a205a214
nikoszz <53112175+nikosszzz@users.noreply.github.com>
2022-10-11 17:38:27 +0300

    feat(updater): implement updater onto dev branch
    Co-authored-by: Norikiru <norikiru.official@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,13 @@ import { config } from "./utils/config";
 import { Logger } from "./utils/logger";
 import { sleep } from "./utils/sleep";
 import { initialize } from "./utils/browser";
+import { updater } from "./utils/updater";
 let page: Page;
 
 (async (): Promise<void> => {
-    Logger.log("STARTUP", "Starting bloxflip-autocrash");
+    await updater();
+
+    Logger.log("STARTUP", `Starting bloxflip-autocrash`);
     Logger.log("SUPPORT", "Support the developers by giving the repo a star! https://github.com/Norikiru/bloxflip-autocrash");
 
     await sleep(1000);
@@ -38,4 +41,4 @@ let page: Page;
     await startCrash();
 })();
 
-export { page };
+export { page }
---
+++ src/utils/updater.ts
@@ -0,0 +1,65 @@
+import { execSync } from "node:child_process";
+import { Logger } from "./logger";
+import inquirer from "inquirer";
+import { join } from "node:path";
+
+async function updater() {
+    let currentHash;
+    let upstreamHash;
+
+    let isOutdated = false;
+
+    const gitDir = join(__dirname, "../../.git");
+
+    try {
+        currentHash = execSync(`git --git-dir "${gitDir}" show -s --format=%h`).toString().trim();
+    } catch (err: any) {
+        return Logger.error("UPDATER", `Unable to fetch current hash, skipping. Is git installed?\n${err}`);
+    }
+
+    try {
+        Logger.log("UPDATER", "Checking for updates...");
+        upstreamHash = execSync(`git --git-dir "${gitDir}" rev-parse --short HEAD`).toString().trim();
+
+        if (currentHash == upstreamHash) isOutdated = false;
+
+        if (currentHash !== upstreamHash) {
+            await inquirer.prompt([
+                {
+                    name: "updaterprompt",
+                    message: "There is an update available! Do you want to update?",
+                    type: "confirm",
+                    default() {
+                        return true
+                    }
+                }
+            ]).then(async (response: { updaterprompt: boolean }) => {
+                if (response.updaterprompt) {
+                    try {
+                        execSync(`git --git-dir "${gitDir}" reset --hard`);
+                        execSync(`git --git-dir "${gitDir}" pull`);
+                        execSync("npm i");
+                        isOutdated = false;
+                        Logger.log("UPDATER", "Updated successfully. Restart the bot to see changes.");
+                        return process.exit()
+                    } catch (err: any) {
+                        isOutdated = true;
+                        Logger.error("UPDATER", err);
+                        Logger.info("UPDATER", "An error occured while updating, ignoring.");
+                    }
+                } else {
+                    isOutdated = true;
+                    Logger.log("UPDATER", "Update skipped.");
+                }
+            });
+        } else {
+            Logger.log("UPDATER", "No updates found.");
+        }
+    } catch (err: any) {
+        isOutdated = true;
+        Logger.error("UPDATER", err);
+        Logger.info("UPDATER", "An error occured while updating, ignoring.");
+    }
+}
+
+export { updater }
