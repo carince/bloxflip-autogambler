5121d3fbf6d7eb5326cbcf7bba3df771d8464b40
Norikiru <norikiru.official@gmail.com>
2022-10-11 23:27:31 +0800

    index: move startup & support log to top

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,11 @@ import { updater } from "./utils/updater";
 let page: Page;
 
 (async (): Promise<void> => {
-    await updater();
-
     Logger.log("STARTUP", "Starting bloxflip-autocrash");
     Logger.log("SUPPORT", "Support the developers by giving the repo a star! https://github.com/Norikiru/bloxflip-autocrash");
-
+    
     await sleep(1000);
+    await updater();
 
     page = await initialize();
 
