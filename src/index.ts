e6cf593ad7abd5f9708ea70aa107366840cb82a6
Norikiru <norikiru.official@gmail.com>
2022-10-11 22:46:33 +0800

    chore: lint all files

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ let page: Page;
 (async (): Promise<void> => {
     await updater();
 
-    Logger.log("STARTUP", `Starting bloxflip-autocrash`);
+    Logger.log("STARTUP", "Starting bloxflip-autocrash");
     Logger.log("SUPPORT", "Support the developers by giving the repo a star! https://github.com/Norikiru/bloxflip-autocrash");
 
     await sleep(1000);
@@ -41,4 +41,4 @@ let page: Page;
     await startCrash();
 })();
 
-export { page }
+export { page };
