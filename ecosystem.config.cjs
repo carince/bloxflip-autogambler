26de359db6a3d96e70b091fa83cc83779da99dd0
Norikiru <norikiru.official@gmail.com>
2022-10-12 20:04:19 +0800

    pm2: add min_uptime

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
     apps: [{
         name: "bloxflip-autocrash",
         script: "run start",
+        min_uptime: 300000,
         max_restarts: 5,
         restart_delay: 5000,
         env: {
