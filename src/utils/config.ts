e4b1c8cb28e843f01728c1eb61d3ded115f7945f
Norikiru <norikiru.official@gmail.com>
2022-10-12 17:55:59 +0800

    config: make fetching cfg async

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,9 +5,6 @@ import { fileURLToPath } from "node:url";
 import { Logger } from "./logger.js";
 import { sleep } from "./sleep.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 interface configInt {
     auth: string;
     tries: number;
@@ -33,22 +30,27 @@ interface configInt {
 
 let config: configInt;
 
-try {
-    (async (): Promise<void> => {
-        config = await json.parse(readFileSync(join(__dirname, "..", "..", "config.json"), "utf-8"));
-        Logger.info("CONFIG", "Fetched config.json.");
+async function fetchCfg() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
 
-        if (config.auth.length < 1000) {
-            Logger.error("TOKEN", "Token length is less than 1000, please put a valid token.");
-        }
+    try {
+        (async (): Promise<void> => {
+            config = await json.parse(readFileSync(join(__dirname, "..", "..", "config.json"), "utf-8"));
+            Logger.info("CONFIG", "Fetched config.json.");
 
-        if (config.tries < 10) {
-            Logger.warn("CONFIG", "It is not recommended to set the tries below 10, exit the script with CTRL+C if you want to make changes.");
-            await sleep(3000);
-        }
-    })();
-} catch (err) {
-    Logger.error("CONFIG", `Unable to read config.json\n${err}`, true);
+            if (config.auth.length < 1000) {
+                Logger.error("TOKEN", "Token length is less than 1000, please put a valid token.");
+            }
+
+            if (config.tries < 10) {
+                Logger.warn("CONFIG", "It is not recommended to set the tries below 10, exit the script with CTRL+C if you want to make changes.");
+                await sleep(3000);
+            }
+        })();
+    } catch (err) {
+        Logger.error("CONFIG", `Unable to read config.json\n${err}`, true);
+    }
 }
 
-export { config };
+export { fetchCfg, config };
