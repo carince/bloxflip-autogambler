ffd82f7b21b6acf8d544c6aaada146efb76e6414
Norikiru <norikiru.official@gmail.com>
2022-10-12 17:07:42 +0800

    data: move loop to main function

--- src/bloxflip/data.ts
+++ src/bloxflip/data.ts
@@ -17,63 +17,59 @@ async function getInfo(): Promise<void> {
     async function loop(): Promise<void> {
         new Promise((): void => {
             setTimeout(async () => {
-                compare();
-                loop();
+                const bfApi = await get("https://rest-bf.blox.land/user");
+
+                const balance: number = Math.round((bfApi.user.wallet + Number.EPSILON) * 100) / 100;
+                let bet: number = balance / Math.pow(2, config.tries);
+                bet = Math.round((bet + Number.EPSILON) * 100) / 100;
+
+                function diffPercent(denominator: number, numerator: number): string {
+                    const string = `${(denominator < numerator ? "-" + ((numerator - denominator) * 100) / denominator : ((denominator - numerator) * 100) / numerator)}`;
+                    return Math.round((parseFloat(string) + Number.EPSILON) * 100) / 100 + "%";
+                }
+
+                function percentageOf(denominator: number, numerator: number): string {
+                    return Math.round(((denominator / numerator * 100) + Number.EPSILON) * 100) / 100 + "%";
+                }
+
+                const gameCount = gameLoss + gameWon;
+                sendWh({
+                    "embeds": [
+                        {
+                            "title": "Hourly Analysis",
+                            "color": 3092790,
+                            "fields": [
+                                {
+                                    "name": "Balance",
+                                    "value": `**Before: ** ${balanceBefore}\n**After: ** ${balance}\n**Difference: ** ${diffPercent(balance, balanceBefore)}`,
+                                    "inline": true
+                                },
+                                {
+                                    "name": "Bet",
+                                    "value": `**Before: ** ${betBefore}\n**After: ** ${bet}\n**Difference: ** ${diffPercent(bet, betBefore)}`,
+                                    "inline": true
+                                },
+                                {
+                                    "name": "Overall Game's",
+                                    "value": `**Joined: ** ${gameWon + gameLoss} games\n**Won: ** ${gameWon} games (${percentageOf(gameWon, gameCount)})\n**Lost: ** ${gameLoss} games (${percentageOf(gameLoss, gameCount)})`,
+                                    "inline": true
+                                }
+                            ],
+                            "footer": {
+                                "text": "bloxflip-autocrash"
+                            },
+                            "thumbnail": {
+                                "url": "https://bloxflip.com/favicon.ico"
+                            }
+                        }
+                    ]
+                });
+
+                Logger.info("DATA", "\tSuccessfully calculated data for analysis.");
+                await loop()
             }, 60 * 60000);
         });
     } if (config.webhook.modules.analytics) await loop();
-}
-
-async function compare(): Promise<void> {
-    const bfApi = await get("https://rest-bf.blox.land/user");
-
-    const balance: number = Math.round((bfApi.user.wallet + Number.EPSILON) * 100) / 100;
-    let bet: number = balance / Math.pow(2, config.tries);
-    bet = Math.round((bet + Number.EPSILON) * 100) / 100;
-
-    function diffPercent(denominator: number, numerator: number): string {
-        const string = `${(denominator < numerator ? "-" + ((numerator - denominator) * 100) / denominator : ((denominator - numerator) * 100) / numerator)}`;
-        return Math.round((parseFloat(string) + Number.EPSILON) * 100) / 100 + "%";
-    }
-
-    function percentageOf(denominator: number, numerator: number): string {
-        return Math.round(((denominator / numerator * 100) + Number.EPSILON) * 100) / 100 + "%";
-    }
-
-    const gameCount = gameLoss + gameWon;
-    sendWh({
-        "embeds": [
-            {
-                "title": "Hourly Analysis",
-                "color": 3092790,
-                "fields": [
-                    {
-                        "name": "Balance",
-                        "value": `**Before: ** ${balanceBefore}\n**After: ** ${balance}\n**Difference: ** ${diffPercent(balance, balanceBefore)}`,
-                        "inline": true
-                    },
-                    {
-                        "name": "Bet",
-                        "value": `**Before: ** ${betBefore}\n**After: ** ${bet}\n**Difference: ** ${diffPercent(bet, betBefore)}`,
-                        "inline": true
-                    },
-                    {
-                        "name": "Overall Game's",
-                        "value": `**Joined: ** ${gameWon+gameLoss} games\n**Won: ** ${gameWon} games (${percentageOf(gameWon, gameCount)})\n**Lost: ** ${gameLoss} games (${percentageOf(gameLoss, gameCount)})`,
-                        "inline": true
-                    }
-                ],
-                "footer": {
-                    "text": "bloxflip-autocrash"
-                },
-                "thumbnail": {
-                    "url": "https://bloxflip.com/favicon.ico"
-                }
-            }
-        ]
-    });
-
-    Logger.info("DATA", "\tSuccessfully calculated data for analysis.");
 }
 
 export { getInfo, balanceBefore };
