b5afff3d1cec90447f706cc99c5802b5f9048600
Norikiru <norikiru.official@gmail.com>
2022-10-13 21:46:19 +0800

    user: add bet check and more entries to webhook

--- src/bloxflip/user.ts
+++ src/bloxflip/user.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import { Logger } from "../utils/logger.js";
 import { sendWh } from "../utils/webhook.js";
 import { get } from "../utils/pfetch.js";
+import { config } from "../utils/config.js";
 
 async function checkAuth(): Promise<void> {
     Logger.info("USER", "\tFetching user information.");
@@ -10,6 +11,12 @@ async function checkAuth(): Promise<void> {
         const res = await get("https://rest-bf.blox.land/user");
 
         if (res.success) {
+            const baseBet = Math.round(((Math.round((res.user.wallet + Number.EPSILON) * 100) / 100) / Math.pow(2, config.tries) + Number.EPSILON) * 100) / 100
+            
+            if (baseBet === 0) {
+                return Logger.error("USER", "\tTries in config is too high causing the bet to be 0", true);
+            }
+
             Logger.log("USER", `\t${chalk.bold("User Information")} \n\t\tUsername: ${res.user.robloxUsername} \n\t\tID: ${res.user.robloxId}\n\t\tBalance: ${Math.round((res.user.wallet + Number.EPSILON) * 100) / 100} R$`);
             sendWh({
                 "embeds": [
@@ -30,6 +37,16 @@ async function checkAuth(): Promise<void> {
                             {
                                 "name": "Balance",
                                 "value": `${Math.round((res.user.wallet + Number.EPSILON) * 100) / 100} R$`,
+                                "inline": true
+                            },
+                            {
+                                "name": "Bet",
+                                "value": `**Tries: **${config.tries}\n**Base Bet: **${baseBet}`,
+                                "inline": true
+                            },
+                            {
+                                "name": "Modules",
+                                "value": `**Rain: **${config.webhook.modules.rain.enabled}, ${config.webhook.modules.rain.minimum} R$\n**Analytics: **${config.webhook.modules.analytics}`,
                                 "inline": true
                             }
                         ],
