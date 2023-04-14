bbc58610f454389639a1eb39239b2304d1b55258
Norikiru <99774021+Norikiru@users.noreply.github.com>
2023-04-14 18:08:16 +0800

    api: start development rain: implement to ws & api

--- src/bloxflip/rain.ts
+++ src/bloxflip/rain.ts
@@ -1,83 +1,63 @@
 import notifier from "node-notifier";
-import { Logger } from "../utils/logger.js";
 import { sendWh } from "../utils/webhook.js";
 import { config } from "../utils/config.js";
-import { get } from "../utils/pfetch.js";
+import { Request, Response } from "express";
 
-async function startRain(): Promise<void> {
-    Logger.info("RAIN", "Starting rain notifier.");
+async function notifyRain(req: Request, res: Response): Promise<void> {
+    const { rain }: any = req.body!
 
-    let notified = false;
-    async function start(): Promise<void> {
-        new Promise((): void => {
-            setTimeout(async (): Promise<void> => {
-                const bfApi = await get("https://rest-bf.blox.land/chat/history");
-                
-                function pingId() {
-                    const idLength = config.webhook.modules.rain.ping_id.length;
-                    if (idLength === 18 || idLength === 19) {
-                        return `<@${config.webhook.modules.rain.ping_id}>`;
-                    } else {
-                        return "";
-                    }
-                }
-                
-                if (bfApi.rain.active) {
-                    if (!notified) {
-                        if (bfApi.rain.prize >= config.webhook.modules.rain.minimum) {
-                            if (config.webhook.modules.rain.os_notifs) {
-                                notifier.notify({
-                                    title: "AutoCrash Rain Notifier",
-                                    message: `Robux: ${bfApi.rain.prize} R$ \nHost: ${bfApi.rain.host} \nTime Remaining: ${bfApi.rain.duration / 60000} minutes`,
-                                    subtitle: "bloxflip-autocrash",
-                                    sound: true
-                                });
-                            }
+    if (rain.prize >= config.webhook.modules.rain.minimum) {
+        function pingId() {
+            const idLength = config.webhook.modules.rain.ping_id.length;
+            if (idLength === 18 || idLength === 19) {
+                return `<@${config.webhook.modules.rain.ping_id}>`;
+            } else {
+                return "";
+            }
+        }
 
-                            sendWh({
-                                "content": pingId(),
-                                "embeds": [
-                                    {
-                                        "title": "Bloxflip Rain Notifier",
-                                        "url": "https://bloxflip.com",
-                                        "color": 3092790,
-                                        "fields": [
-                                            {
-                                                "name": "Prize",
-                                                "value": `${bfApi.rain.prize} R$`,
-                                                "inline": true
-                                            },
-                                            {
-                                                "name": "Host",
-                                                "value": bfApi.rain.host,
-                                                "inline": true
-                                            },
-                                            {
-                                                "name": "Time Remaining",
-                                                "value": `${bfApi.rain.duration / 60000} minutes`,
-                                                "inline": true
-                                            }
-                                        ],
-                                        "footer": {
-                                            "text": "bloxflip-autocrash"
-                                        }
-                                    }
-                                ]
-                            });
-                        } else {
-                            Logger.info("RAIN", "Prize is not greater or equal than the minimum value set in the config, ignoring...");
+        if (config.webhook.modules.rain.os_notifs) {
+            notifier.notify({
+                title: "AutoCrash Rain Notifier",
+                message: `Robux: ${rain.prize} R$ \nHost: ${rain.host} \nTime Remaining: ${rain.duration / 60000} minutes`,
+                subtitle: "bloxflip-autocrash",
+                sound: true
+            });
+        }
+
+        sendWh({
+            "content": pingId(),
+            "embeds": [
+                {
+                    "title": "Bloxflip Rain Notifier",
+                    "url": "https://bloxflip.com",
+                    "color": 3092790,
+                    "fields": [
+                        {
+                            "name": "Prize",
+                            "value": `${rain.prize} R$`,
+                            "inline": true
+                        },
+                        {
+                            "name": "Host",
+                            "value": rain.host,
+                            "inline": true
+                        },
+                        {
+                            "name": "Time Remaining",
+                            "value": `${rain.duration / 60000} minutes`,
+                            "inline": true
                         }
-
-                        notified = true;
+                    ],
+                    "footer": {
+                        "text": "bloxflip-autocrash"
                     }
-                } else {
-                    notified = false;
                 }
-
-                await start();
-            }, 5000);
+            ]
         });
-    } await start();
+    }
+
+    res.sendStatus(200)
 }
 
-export { startRain };
+export { notifyRain };
