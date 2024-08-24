dd4cc39e69623c642cf8b24cfa6d30b03a0a3c32
carince <99774021+carince@users.noreply.github.com>
2024-08-24 13:40:13 +0800

    Revert "browser: hopefully fix some crashes"
    This reverts commit a32b839b8fab677554ceb5dfef7d6f132e6b4c81.

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -19,7 +19,7 @@ async function startBrowser(): Promise<void> {
                 headless: config.debugging.headless,
                 defaultViewport: { width: 1920, height: 1080 },
                 args: [
-                    "--start-maximized",
+                    "--start-maximized", "--single-process",
                     `--disable-extensions-except=${pathToExtension}`,
                     `--load-extension=${pathToExtension}`,
                 ],
